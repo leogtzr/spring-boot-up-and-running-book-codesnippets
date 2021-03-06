08727c5f5725edfebea619f0cb226e1cd61410f1
leogtzr <leogutierrezramirez@gmail.com>
2021-03-06 10:08:35 -0700

    add little Spring Security project to play with

---
+++ person-web-security/src/main/java/com/person/web/domain/Person.java
@@ -0,0 +1,23 @@
+package com.person.web.domain;
+
+import lombok.*;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@ToString
+@EqualsAndHashCode
+@Builder
+@Entity
+// The annotation below are required if we want to use @Builder.
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Person {
+    @Id
+    private Long id;
+    private int age;
+    private String name;
+}
