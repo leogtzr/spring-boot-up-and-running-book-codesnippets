a492fca69f8781e4ad1a516d11d436d241fd0df0
leogtzr <leogutierrezramirez@gmail.com>
2021-03-06 17:49:38 -0700

    add password encoder

--- person-web-security/src/main/java/com/person/web/configuration/SecurityConfiguration.java
+++ person-web-security/src/main/java/com/person/web/configuration/SecurityConfiguration.java
@@ -5,22 +5,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfiguration {
 
+    // A default one:
+    private final PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
     @Bean
     public UserDetailsService authentication() {
         final UserDetails leo = User.builder()
                                     .username("leo")
-                                    .password("lein23")
+                                    .password(passwordEncoder.encode("lein23"))
                                     .roles("USER", "ADMIN")
                 .build();
 
         final UserDetails brenda = User.builder()
                 .username("brenda")
-                .password("brenda23")
+                .password(passwordEncoder.encode("brenda23"))
                 .roles("USER")
                 .build();
 
